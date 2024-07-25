0e9fdad4e2fec6fc2fa3f8f6099390c4660388e9
Francisco17S <js698290@gmail.com>
2024-07-25 09:39:25 -0500

    Examen del segundo parcial NestJs

--- Examen/dignidad/dist/candidato/candidato.gateway.d.ts
+++
@@ -1,7 +0,0 @@
-import { Server, Socket } from 'socket.io';
-import { Candidato } from './entities/candidato.entity';
-export declare class CandidatoGateway {
-    server: Server;
-    private clients;
-    handleMessage(client: Socket, payload: Candidato): void;
-}
